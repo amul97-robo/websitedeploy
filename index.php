f71f7b2cfbeeb908a8bc91bbad04a4a76aef4501
amul97-robo <59188657+amul97-robo@users.noreply.github.com>
2020-03-27 23:56:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
 <?php include_once("index.html"); 
+
+<html>
+<body bgcolor="black">
+
+<font color="darkred">
+<center>Your Principal is $<?php echo $_POST["principal"]; ?>.
+
+Your Interest Rate is <?php echo $_POST["inrate"]; ?>%.
+
+Your Loan is for 
+<?php
+if ($_POST["time"]==1) 
+  echo $_POST["time"] . " year.";
+else 
+  echo $_POST["time"] . " years.";
+?>
+
+Your total is:
+<?php
+$p=$_POST["principal"];
+$i=($_POST["inrate"]/100);
+$t=$_POST["time"];
+$n=365;
+$a=(1 + ($i/365));
+$b=($n*$t);
+$total=$p*(pow($a, $b));
+if
+(
+$p=$_POST["principal"] &&
+$i=($_POST["inrate"]/100) &&
+$t=$_POST["time"] &&
+$n=365 &&
+$a=(1 + ($i/365)) &&
+$b=($n*$t) &&
+$total=$p*(pow($a, $b))
+)
+echo $total
+?>
+</center>
+
+
+</font>
+</body>
+</html>
+
 ?>
